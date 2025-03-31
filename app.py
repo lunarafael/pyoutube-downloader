7c9aa09e9e4e9d95067b309d9846962460ee89fd
Rafael Luna <kenkorio23@gmail.com>
2025-03-31 20:44:41 -0300

    Added template folder for Flask application

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from src.middleware.routes import configure_routes
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='src/templates')
 
 configure_routes(app)
 
