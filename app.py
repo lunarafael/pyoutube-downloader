f5df7ba57080d294909855f699e49270070fcc7e
Rafael Luna <kenkorio23@gmail.com>
2025-03-31 20:55:35 -0300

    Updated app.py to configure dynamic port and host for Flask application

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from src.middleware.routes import configure_routes
+import os
 
 app = Flask(__name__, template_folder='src/templates')
 
 configure_routes(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
