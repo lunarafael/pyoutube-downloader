3ed26f3ba5081fcf91b49d1e25c726bf895f57f0
Rafael Luna <kenkorio23@gmail.com>
2025-03-31 21:16:31 -0300

    Added custom HTTP headers for requests

--- src/libs/downloader.py
+++ src/libs/downloader.py
@@ -1,7 +1,14 @@
 from pytubefix import YouTube
+from pytubefix.request import HTTPRequest
 from io import BytesIO
 import os
 from moviepy.editor import VideoFileClip, AudioFileClip
+import requests
+
+HTTPRequest._headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'Accept-Language': 'en-US,en;q=0.9',
+}
 
 class VideoDownloader:
     def __init__(self, url):
