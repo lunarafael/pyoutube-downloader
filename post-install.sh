de4f45c131da2f81496a894dc35643ef9a127060
Rafael Luna <kenkorio23@gmail.com>
2025-04-08 12:20:26 -0300

    Added Node.js installation and package setup to post-install script

--- post-install.sh
+++ post-install.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+
+# Instala o Node.js (Linux/Ubuntu)
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Verifica a instalação
+node -v
+npm -v
+
+# Instala o pacote necessário para o pytubefix
+npm install youtube-po-token-generator
+
 python -m nodeenv -p
